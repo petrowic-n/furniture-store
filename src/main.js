dd6a399ec81319ac253f877b5ed9acb53f80d162
petrowic-n <nemanjapetrovic358@gmail.com>
2025-11-13 23:31:10 +0100

    section: offer, responsive

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/offer.css";
 import "../styles/utils.css";
 
 import script from "./utils/script";
